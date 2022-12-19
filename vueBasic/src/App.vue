79fea83ecb24a74e975878fbc7ab5f9edbfce8a7
realseok2 <kts4315@naver.com>
2022-12-19 15:00:31 +0900

    vue study setting

---
+++ vueBasic/src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+  import HelloWorld from './components/HelloWorld.vue'
+
+  
+</script>
+
+<script>
+  // new Vue({
+  //   el: '#test',
+  //   data: {              
+  //     testData:'WTF!!!'
+  //   }
+  // })
+
+  export default{
+    data() {
+      return {
+        testData : 'WTF'
+      }
+    }
+  }
+</script>
+
+<template>
+  <div id="test">
+    <h1>vue study</h1>
+    <p> {{ testData }} </p>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
