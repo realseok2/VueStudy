a2d5c7a3279d8c23bb867a56fb897a9019d3b850
realseok2 <kts4315@naver.com>
2022-12-22 10:05:11 +0900

    todoList Commit

--- vueBasic/src/components/TodoList.vue
+++ vueBasic/src/components/TodoList.vue
@@ -1,11 +1,99 @@
 <script>
 export default {
-  data() {},
+  el : "todo",
+  data() {
+    return {
+      state   : true,
+      todo    : "",
+      userNm  : "",
+      inputNm : "",
+      todoList: [],
+    }
+  },
+
+  watch: {
+      state (state) {
+          //data -> state 데이터가 변경될 때 실행
+          console.log('state: ' + state)
+          this.state = state;
+      },
+  },
+
+  methods : {
+    //엔터 눌렀을 때
+    setNm() {
+      this.userNm = this.inputNm;
+      console.log(this.state);
+    },
+
+    //submit 클릭했을 때
+    getNm() {
+      this.userNm = this.inputNm;
+      if(this.userNm == '') {
+        console.log("No Name");
+      } else {
+        console.log("ㅋㅋㅋㅋㅋ");
+
+        this.todoList.push(this.userNm);
+        console.log(this.todoList);
+
+        // axios.get('211.47.239.80:8080/get-todo/홍길동')
+        //     .then((res) => {
+        //         alert(res);
+        //         console.log(res);
+        //     })
+        //     .catch((error) => {
+        //       console.log(error);
+        //     })
+        //     .finally(() => {
+        //       console.log("last");
+        //     });
+      }
+    },
+
+    delNm() {
+      console.log("delete");
+      // this.todoId.state = false;
+
+      // if(this.state == true) {
+      //   this.state = false;
+      //   return;
+      // } else if(this.state == false) {
+      //   this.state = true;
+      //   return;
+      // }
+      this.todoList.forEach(state => {
+        console.log("state = " + state);
+        // this.state = false;
+      });
+
+
+    },
+  }
 };
 </script>
 
 <template>
   <div id="todo">
-    <input type="text" v-model="todo" @keyup="addTodo"> 
+    <div>
+      <input type="text" v-model="inputNm" @keyup.enter="setNm">
+    </div>
+
+    <br>
+    
+    <div>
+      [ {{ userNm }} ]
+    </div>
+
+    <div class="todoArr" v-for="todoList in todoList" @click="delNm">
+      {{ todoList }}
+    </div>
+
+    <br>
+    
+    <div>
+      <button type="button" @click="getNm">submit</button>
+    </div>
+
   </div>
 </template>
